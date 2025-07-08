6a2b928a4d1b1e1ffb1ed03bda034a32a6f8532a
huytdn <saolailatoivay991@gmail.com>
2025-07-08 15:49:53 +0700

    env(khong push env len git)

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 8080;
+require("dotenv").config();
+
+const port = process.env.PORT || 8888;
+const hostname = process.env.HOST_NAME;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -10,6 +13,6 @@ app.get("/", (req, res) => {
   res.render("sample.ejs");
 });
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
 });
