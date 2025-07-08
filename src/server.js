458b6d775ad2fba109ec14fb4fb350294e9d6d45
huytdn <saolailatoivay991@gmail.com>
2025-07-08 23:08:49 +0700

    static files

--- src/server.js
+++ src/server.js
@@ -9,6 +9,8 @@ const hostname = process.env.HOST_NAME;
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
   res.render("sample.ejs");
 });
