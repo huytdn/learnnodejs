8e5280aa213c24daabdffbc479ece235ac0fbaf9
huytdn <saolailatoivay991@gmail.com>
2025-07-09 22:01:07 +0700

    cau hinh lai database

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
-const mysql = require("mysql2");
+const connection = require("./config/database");
 
 const app = express();
 const port = process.env.PORT || 8888;
@@ -13,17 +13,8 @@ configViewEngine(app);
 
 app.use("/", webRoutes);
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3307,
-  user: "root",
-  password: "123456",
-  database: "nhathuy",
-});
-
 connection.query("select * from Users u", function (err, results, fields) {
   console.log(results); // results contains rows returned by server
-  console.log(fields); // fields contains extra meta data about results, if available
 });
 
 app.listen(port, hostname, () => {
