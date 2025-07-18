882fce7b6eff77cc3ed570af94bd52a5d22f637b
huytdn <saolailatoivay991@gmail.com>
2025-07-18 10:45:51 +0700

    design navbar

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,12 +1,11 @@
 const connection = require("../config/database");
 
 const getHomePage = (req, res) => {
-  let users = [];
-  connection.query("select * from Users u", function (err, results, fields) {
-    users = results;
-    console.log(results); // results contains rows returned by server
-    res.send(JSON.stringify(users));
-  });
+  return res.render("home.ejs");
 };
 
-module.exports = connection;
+const getFriend = (req, res) => {
+  res.render("sample.ejs");
+};
+
+module.exports = { getHomePage, getFriend };
