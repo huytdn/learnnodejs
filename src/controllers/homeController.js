22a7cf841104a6bc4b7bb63ceb386c00cefb02ea
huytdn <saolailatoivay991@gmail.com>
2025-07-19 09:54:00 +0700

    query list users

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,9 @@
 const connection = require("../config/database");
+const { getAllUsers } = require("../services/CRUDService");
 
-const getHomePage = (req, res) => {
-  return res.render("home.ejs");
+const getHomePage = async (req, res) => {
+  let results = await getAllUsers();
+  return res.render("home.ejs", { listUsers: results });
 };
 
 const getFriend = (req, res) => {
